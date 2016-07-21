9461ae6ecce6875cfe78a07abb107fa478e9629f
CompileYouth <CompileYouth@163.com>
2016-07-21 09:53:57 +0800

    Use afterInit instead of init

--- src/mb/app/Application.js
+++ src/mb/app/Application.js
@@ -3,15 +3,15 @@ import AdaptiveApplication from "sap/a/app/Application";
 import MapView from "../map/MapView";
 
 export default class Application extends AdaptiveApplication {
-    init() {
-        super.init();
+    afterInit() {
+        super.afterInit();
         this.addStyleClass("mb-app");
         this._initMapView();
     }
 
     _initMapView() {
         this.mapView = new MapView("map-view", {
-            defaultZoom: 10
+            defaultZoom: 8
         });
         this.addSubview(this.mapView);
     }
