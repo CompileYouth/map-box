fc0301c0218cac8fcfb8a66ad98da28988f05a2c
CompileYouth <CompileYouth@163.com>
2016-08-01 09:51:21 +0800

    Modify style.

--- src/mb/view/ODSearchView.js
+++ src/mb/view/ODSearchView.js
@@ -3,7 +3,7 @@ import View from "sap/a/view/View";
 export default class ODSearchView extends View {
     metadata = {
         properties: {
-            type: { type: "string" }
+            type: { type: "string", defaultValue: "car" }
         },
         events: {
             searchRoute: {}
@@ -20,14 +20,15 @@ export default class ODSearchView extends View {
     afterInit() {
         super.afterInit();
 
+        this.setType(this.getType());
     }
 
     _initLayout() {
         this.$container.append(`
             <ul class="tabs">
-                <li class="car"><i class="iconfont icon-car"></i>驾车</li>
-                <li class="bus"><i class="iconfont icon-bus"></i>公交</li>
-                <li class="walking"><i class="iconfont icon-people-walking"></i>步行</li>
+                <li class="car" data-type="car"><i class="iconfont icon-car"></i>驾车</li>
+                <li class="bus" data-type="bus"><i class="iconfont icon-bus"></i>公交</li>
+                <li class="walking" data-type="walking"><i class="iconfont icon-people-walking"></i>步行</li>
             </ul>
             <div class="line-search">
                 <div class="line-search-left">
@@ -45,9 +46,24 @@ export default class ODSearchView extends View {
 
         const $searchBtn = this.$(".line-search-submit button");
         $searchBtn.on("click", this._searchBtn_click.bind(this));
+        this.$tab = this.$(".tabs");
+        this.$tab.on("click", "li", this._tab_click.bind(this));
+    }
+
+    setType(type) {
+        this.setProperty("type", type);
+        if (type !== null) {
+            this.$tab.children("li").removeClass("active");
+            this.$tab.find(`.${type}`).addClass("active");
+        }
     }
 
     _searchBtn_click(e) {
         this.fireSearchRoute();
+    }
+
+    _tab_click(e) {
+        const type = $(e.currentTarget).attr("data-type");
+        this.setType(type);
     }
 }
